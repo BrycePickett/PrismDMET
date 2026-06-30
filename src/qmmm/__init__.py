7e186bb2335c0a39abcc3386de70b66f8fda62d4
BrycePickett <brycepickett22@gmail.com>
2026-06-29 21:42:20 -0400

    Move defect_builder into qmmm subpackage, delete dead QC-DMET ring_helper

--- src/qmmm/__init__.py
+++ src/qmmm/__init__.py
@@ -1,6 +1,7 @@
 """Public API for the PrismDMET QM/MM embedding module."""
 
 from .builder          import QMMMBuilder
+from .defect_builder    import DefectBuilder
 from .cluster          import (
     QMMMCluster, AtomRecord,
     REGION_QM, REGION_ECP, REGION_MM, REGION_GHOST,
@@ -12,6 +13,8 @@ from .pyscf_interface  import build_mol, build_meanfield
 __all__ = [
     # Main builder
     'QMMMBuilder',
+    # Lightweight defect-region selector from XYZ pairs
+    'DefectBuilder',
     # Data objects
     'QMMMCluster',
     'AtomRecord',
